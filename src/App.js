0e4efb2e9c4e35aa9f69b522aa89cfbca6cc9f19
bharat1999 <bharatmalik1999@gmail.com>
2023-03-25 22:21:43 +0530

    project initialised and views added

--- src/App.js
+++ src/App.js
@@ -1,49 +1,34 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
-import "./styles/Home.css";
+import { ConnectWallet, useSDK } from "@thirdweb-dev/react";
+import "./assets/css/nucleo-icons.css";
+import "./assets/css/scss/blk-design-system-react.scss";
+import "./assets/css/demo.css";
+import "./App.scss";
+import { Route, Routes } from "react-router-dom";
+import UserState from "./context/UserState";
+import Landing from "./views/Landing";
+import LoginDonor from "./views/DonorLogin";
+import Login from "./views/Login";
+import Register from "./views/Register";
+import BloodbankHome from "./views/BloodbankHome";
+import BloodCollection from "./views/BloodCollection";
+import UpdateStatus from "./views/UpdateStatus";
+import HospitalHome from "./views/HospitalHome";
 
 export default function Home() {
+  const sdk = useSDK();
+
   return (
-    <div className="container">
-      <main className="main">
-        <h1 className="title">
-          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
-        </h1>
-
-        <p className="description">
-          Get started by configuring your desired network in{" "}
-          <code className="code">src/index.js</code>, then modify the{" "}
-          <code className="code">src/App.js</code> file!
-        </p>
-
-        <div className="connect">
-          <ConnectWallet />
-        </div>
-
-        <div className="grid">
-          <a href="https://portal.thirdweb.com/" className="card">
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className="card">
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a href="https://portal.thirdweb.com/templates" className="card">
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
-      </main>
-    </div>
+    <UserState>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login-donor" element={<LoginDonor />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/bloodbank-home" element={<BloodbankHome />} />
+        <Route path="/blood-collection" element={<BloodCollection />} />
+        <Route path="/update-status" element={<UpdateStatus />} />
+        <Route path="/hospital-home" element={<HospitalHome />} />
+      </Routes>
+    </UserState>
   );
 }
